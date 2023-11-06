1e847470bb9c08c10acde371f0fd8b6d79fbed76
dominikauekat <dominika.marcak@edu.uekat.pl>
2023-11-06 15:00:51 +0100

    dodanie noego pliku

---
+++ zadanie 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 14:21:23 2023
+
+@author: student
+"""
+
+def przywitaj(name, surname):
+    return f"Cześć {name} {surname}!"
+
+powitanie = przywitaj("Dominika", "Marcak")
+
+print(powitanie)
